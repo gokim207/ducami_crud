2b99dbde6a3d7d54ee7d5c02b4ebac309d4a9d4b
dgsw43 <dgsw43@dgsw43ui-MacBookAir.local>
2024-07-23 20:38:20 +0900

    수정

--- src/main/java/com/example/board/crud/Application.java
+++ src/main/java/com/example/board/crud/Application.java
@@ -3,11 +3,15 @@ package com.example.board.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-@SpringBootApplication
-@EnableJpaAuditing
 
+// 중요! 실수로 yml을 지운다면 형벌이 내려짐.
+@SpringBootApplication
+@EnableJpaAuditing // jpa auditing 활성화
+// 2트 : 브랜치 바꿈
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
--- src/main/java/com/example/board/crud/dto/Response/BoardResponse.java
+++ src/main/java/com/example/board/crud/dto/Response/BoardResponse.java
@@ -1,6 +1,5 @@
 package com.example.board.crud.dto.Response;
 
-
 import com.example.board.crud.domain.BoardEntity;
 import jdk.jshell.Snippet;
 import lombok.*;
@@ -8,13 +7,13 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Builder
-@Getter
+@Getter // 둘이 바꿔
 @AllArgsConstructor
 public class BoardResponse {
     private Long id;
     private String title;
     private String content;
-    private LocalDateTime createDate;
+    private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
     public static BoardResponse of(BoardEntity boardEntity){
@@ -23,7 +22,9 @@ public class BoardResponse {
                 .title(boardEntity.getTitle())
                 .content(boardEntity.getContent())
                 .createdDate(boardEntity.getCreatedDate())
-                .modifiedDate(boardEntity.getModifiedDate().build());
+                .modifiedDate(boardEntity.getModifiedDate()).build();
     }
+
+
 
 }
